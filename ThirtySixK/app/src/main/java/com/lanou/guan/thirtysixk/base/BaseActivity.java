f7ed3c4614f42ffc898a1e94d03333b063df738e
关玉鑫 <2548197052@qq.com>
2016-06-17 14:13:31 +0800

    创建工程

---
+++ ThirtySixK/app/src/main/java/com/lanou/guan/thirtysixk/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.lanou.guan.thirtysixk.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/6/17.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+}
